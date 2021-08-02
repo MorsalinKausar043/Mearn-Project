f51e18b46b59d67ee7ebdab19bfd6cffdb28a7e2
Morsalin Kausar <morsalinkausar043@gmail.com>
2021-08-02 20:11:50 +0600

    token generat done

--- server/models/conn.js
+++ server/models/conn.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -26,8 +28,47 @@ const userSchema = new mongoose.Schema({
     cpassword: {
         type: String,
         required : true
+    },
+    tokens : [{
+        token: {
+            type: String,
+            required : true
+        }
+    }],
+    data: {
+        type: Date,
+        default : Date.now
     }
 });
+
+userSchema.methods.tokenGenerate = async function () {
+    try
+    {
+        const token = await jwt.sign({ _id: this._id.toString() }, process.env.SECRET_KEY);
+        this.tokens = this.tokens.concat({ token });
+        await this.save;
+        return token;
+        
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+userSchema.pre("save", async function (next) {
+    try
+    {
+        if (this.isModified("password"))
+        {
+            this.password = await bcrypt.hash(this.password, 10);
+            this.cpassword = await bcrypt.hash(this.cpassword, 10);
+        }
+
+        next();
+        
+    } catch (error) {
+        console.log(error)
+    }
+})
 
 const User = new mongoose.model("USER", userSchema);
 
--- server/router.js
+++ server/router.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = new express.Router();
 const User = require("./models/conn");
+const bcrypt = require("bcryptjs");
 
 
 // router data get 
@@ -17,9 +18,9 @@ router.get("/contact", (req, res) => {
     res.send("hello world this is contact page!");
 });
 
-router.get("/login", (req, res) => {
-    res.send("hello world this is login page!");
-});
+// router.get("/login", (req, res) => {
+//     res.send("hello world this is login page!");
+// });
 
 router.get("/signup", (req, res) => {
     res.send("hello world this is signup page!");
@@ -39,24 +40,65 @@ router.post("/registration", async (req, res) => {
     
     try
     {
-        const UserExits = await User.findOne({ email});
+        const UserExits = await User.findOne({ email });
         
         if (UserExits)
         {
             console.log("2 time registration form log in!");
-            res.status(422).json( {error : "2 time registration form log in!"});
+            res.status(422).json({ error: "2 time registration form log in!" });
         }
-        
-        const PostUserData = new User({name , email ,phone , work , password , cpassword});
-        await PostUserData.save();
-        res.status(201).json({massage : "save successfull!"});
-
-        
+        else if (password !== cpassword)
+        {
+            res.status(422).json({ error: "invalid details" });
+        } else
+        {
+            const PostUserData = new User({ name, email, phone, work, password, cpassword });
+            const token = await PostUserData.tokenGenerate();
+            res.cookie("jwt", token, {
+                expires: new Date(Date.now() + 200000),
+                httpOnly: true
+            });
+            await PostUserData.save();
+            res.status(201).json({massage : "save successfull!"}); 
+        }
+                    
     } catch (error) {
-        res.status(422).json({error : error});
+        res.status(422).json({ error: error });
+        console.log(error)
     }
 });
 
+// login post 
+
+router.post("/login", async (req, res) => {
+    try
+    {
+        const { email, password } = req.body;
+
+        if (!email || !password)
+        {
+            res.status(422).json({ error: "plz fill the all input form!" });
+        } else
+        {
+            const UserLogData = await User.findOne({});
+            const isMatch = await bcrypt.compare(password, UserLogData.password);
+
+            const token = await UserLogData.tokenGenerate();
+            res.cookie("jwt", token, {
+                expires: new Date(Date.now() + 200000),
+                httpOnly: true
+                // secure : true
+            });
+
+            isMatch ? res.status(201).json({ massage: "Successful login!" }) : res.status(201).json({ error: "unsuccessful error!" });
+        }
+        
+    } catch (error)
+    {
+        res.status(422).json({ error: "invalid details!" });
+        console.log(error);
+    }
+})
 
 // router exporting 
 
